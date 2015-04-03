489ebf3f8f19b83ab82265791ee49b075eb17347
luohaha <18810541851@163.com>
2015-04-03 11:55:52 +0800

    fix bug

--- app/src/main/java/com/example/root/libapp_v1/Fragment/FiveFragment.java
+++ app/src/main/java/com/example/root/libapp_v1/Fragment/FiveFragment.java
@@ -1,30 +1,28 @@
 package com.example.root.libapp_v1.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 import com.example.root.libapp_v1.HeadBar.HeadBar;
 import com.example.root.libapp_v1.R;
-import com.example.root.libapp_v1.SpinnerButton.SpinnerButton;
-
-import java.util.ArrayDeque;
 
 /**
  * Created by root on 15-3-26.
  */
-public class FiveFragment extends FatherFragment {
+public class FiveFragment extends FatherFragment implements OnClickListener{
     //public String initContent() {
       //  return "five!!!";
     //}
+    private PopupWindow popupwindow;
     private HeadBar headBar;
-    private SpinnerButton spinnerButton;
+    private Button button;
+    private View customView;
     String[] strs = new String[] {"no1", "no2", "no3"};
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -33,29 +31,59 @@ public class FiveFragment extends FatherFragment {
       //  TextView textView = (TextView) view.findViewById(R.id.txt_content);
         headBar = (HeadBar)this.getActivity().findViewById(R.id.head_bar);
         headBar.setTitleText("扫一扫");
-        spinnerButton = (SpinnerButton) view.findViewById(R.id.custom_sinnper);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.select_dialog_item, strs);
-        spinnerButton.setAdapter(adapter);
-        spinnerButton.setOnItemSeletedListener(new SpinnerButton.OnItemSeletedListener() {
-            @Override
-            public void onItemSeleted(AdapterView<?> parent, View view, int position, long id) {
-                Object obj = parent.getItemAtPosition(position);
-                Log.i("onitemselected ", obj + "");
-            }
-        });
+        button = (Button)view.findViewById(R.id.button1);
+        button.setOnClickListener(this);
+        customView = inflater.inflate(R.layout.popview_item, null);
+
         return view;
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        Log.i("static onkeyDowm", "yes");
-        if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
-        {
-            if(spinnerButton.isShowPopup())
-            {
-                spinnerButton.dismiss();
-                return true;
-            }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                if (popupwindow != null && popupwindow.isShowing()) {
+                    popupwindow.dismiss();
+                    return;
+                }
+                else {
+                    initmPopupWindowView();
+                    popupwindow.showAsDropDown(v, 0 ,5);
+
+                }
+                break;
+            default:
+                break;
         }
-        return getView().onKeyDown(keyCode, event);
+    }
+
+    public void initmPopupWindowView() {
+
+        // // 获取自定义布局文件pop.xml的视图
+        // 创建PopupWindow实例,200,150分别是宽度和高度
+        popupwindow = new PopupWindow(customView, 200, 150);
+        // 设置动画效果 [R.style.AnimationFade 是自己事先定义好的]
+        popupwindow.setAnimationStyle(R.style.AnimationFade);
+        // 自定义view添加触摸事件
+        customView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (popupwindow != null && popupwindow.isShowing()) {
+                    popupwindow.dismiss();
+                    popupwindow = null;
+                }
+
+                return false;
+            }
+        });
+
+        /** 在这里可以实现自定义视图的功能 */
+        Button btton2 = (Button) customView.findViewById(R.id.button2);
+        Button btton3 = (Button) customView.findViewById(R.id.button3);
+        Button btton4 = (Button) customView.findViewById(R.id.button4);
+        btton2.setOnClickListener(this);
+        btton3.setOnClickListener(this);
+        btton4.setOnClickListener(this);
+
     }
 }
