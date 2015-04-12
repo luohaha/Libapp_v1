8d146df33a0d3aa239c8b1cebc57debf22225bc9
luohaha <18810541851@163.com>
2015-04-12 11:41:04 +0800

    create login activity

---
+++ app/src/main/java/com/example/root/libapp_v1/UserLogin/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.root.libapp_v1.UserLogin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by Yixin Luo on 15-4-12.
+ * Today is a beautiful day!!
+ */
+public class LoginActivity extends Activity {
+    /**
+     * get the user input message
+     * */
+    private String mInputUserNumber;
+    private String mInputUserPassword;
+
+    /**
+     * buttons, view and images
+     * */
+    private Button mLoginButton;
+    private ImageButton mDownButton;
+    private ImageButton mDeleteButton;
+    private TextView mNumberTextView;
+    private TextView mPasswordTextView;
+    private EditText mUserNumberEditText;
+    private EditText mUserPasswordEditText;
+
+    private ListView mListView;
+
+    private boolean isListViewVisiable;
+    private boolean isDownButtonUp;
+
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
