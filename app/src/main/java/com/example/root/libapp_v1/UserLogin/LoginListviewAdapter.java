808ab8a7d05fc02ea9ed42cab32202776807dfe7
luohaha <18810541851@163.com>
2015-04-13 11:56:15 +0800

    add some code

--- app/src/main/java/com/example/root/libapp_v1/UserLogin/LoginListviewAdapter.java
+++ app/src/main/java/com/example/root/libapp_v1/UserLogin/LoginListviewAdapter.java
@@ -1,9 +1,15 @@
 package com.example.root.libapp_v1.UserLogin;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.root.libapp_v1.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,17 +23,48 @@ public class LoginListviewAdapter extends BaseAdapter{
     private Context mContext;
     private ArrayList<HashMap<String, Object>> mList;
     private int mItemLayout;
+    /**
+     * get the user's number and image
+     * */
+    private String[] mGetNumber;
+    private Integer[] mGetImage;
 
     public LoginListviewAdapter(Context mContext, ArrayList<HashMap<String, Object>> arrayList,
-                                int itemLayout) {
+                                int itemLayout, String[] getNumber, Integer[] getImage) {
         this.mContext = mContext;
         this.mList = arrayList;
         this.mItemLayout = itemLayout;
+        this.mGetImage = getImage;
+        this.mGetNumber = getNumber;
+
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(mItemLayout, null);
+            holder = new ViewHolder();
+            holder.userButton = (ImageButton) convertView.findViewById(R.id.login_deleteButton);
+            holder.userNumber = (TextView) convertView.findViewById(R.id.login_userQQ);
+            holder.userImage = (ImageView) convertView.findViewById(R.id.login_userPhoto);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        /**
+         * set the holder's value
+         * */
+        holder.userImage.setImageResource((Integer) mList.get(position).get("userImage"));
+        holder.userNumber.setText((String) mList.get(position).get("userNumber"));
+        holder.userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mList.remove(position);
+                if (position)
+            }
+        });
+        return convertView;
     }
 
     @Override
