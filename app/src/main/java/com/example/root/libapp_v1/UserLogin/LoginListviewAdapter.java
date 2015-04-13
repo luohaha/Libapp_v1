9980e1551f85eda710abdd70e42b56a50bb6baef
luohaha <18810541851@163.com>
2015-04-13 10:44:51 +0800

    new view holder

--- app/src/main/java/com/example/root/libapp_v1/UserLogin/LoginListviewAdapter.java
+++ app/src/main/java/com/example/root/libapp_v1/UserLogin/LoginListviewAdapter.java
@@ -1,11 +1,47 @@
 package com.example.root.libapp_v1.UserLogin;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Yixin Luo on 15-4-13.
  * where?
+ * here
  */
 public class LoginListviewAdapter extends BaseAdapter{
-    
+    private Context mContext;
+    private ArrayList<HashMap<String, Object>> mList;
+    private int mItemLayout;
+
+    public LoginListviewAdapter(Context mContext, ArrayList<HashMap<String, Object>> arrayList,
+                                int itemLayout) {
+        this.mContext = mContext;
+        this.mList = arrayList;
+        this.mItemLayout = itemLayout;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
---
+++ app/src/main/java/com/example/root/libapp_v1/UserLogin/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.root.libapp_v1.UserLogin;
+
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Yixin Luo on 15-4-13.
+ * hahaha
+ */
+public class ViewHolder {
+    public ImageView userImage;
+    public TextView userNumber;
+    public ImageButton userButton;
+}
