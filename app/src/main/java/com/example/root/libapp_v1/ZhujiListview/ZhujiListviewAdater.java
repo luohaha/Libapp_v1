efcb0548726358be5fcfbc2452786d042d01c853
luohaha <18810541851@163.com>
2015-04-17 09:47:16 +0800

    add view holder

--- app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiListviewAdater.java
+++ app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiListviewAdater.java
@@ -24,17 +24,17 @@ public class ZhujiListviewAdater extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return mList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
---
+++ app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.root.libapp_v1.ZhujiListview;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Yixin on 15-4-17.
+ */
+public class ZhujiViewHolder {
+    public ImageView zhujiMainImg;
+    public ImageView zhujiHeadImg;
+    public TextView zhujiName;
+    public TextView zhujiUpdateTime;
+    public
+}
