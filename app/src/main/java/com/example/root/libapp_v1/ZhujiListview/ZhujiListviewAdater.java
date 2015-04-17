92039e8d64f92639b6aa6748c30e8ad7594827e8
luohaha <18810541851@163.com>
2015-04-17 14:58:27 +0800

    finishing adapter

--- app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiListviewAdater.java
+++ app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiListviewAdater.java
@@ -3,7 +3,16 @@ package com.example.root.libapp_v1.ZhujiListview;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
+import com.beardedhen.androidbootstrap.FontAwesomeText;
+import com.example.root.libapp_v1.R;
+import com.example.root.libapp_v1.UserLogin.ViewHolder;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -16,10 +25,11 @@ public class ZhujiListviewAdater extends BaseAdapter {
 
     private Context mContext;
     private ArrayList<Map<String, Object>> mList;
-
+    private LayoutInflater mInflater;
     public ZhujiListviewAdater(Context context, ArrayList<Map<String, Object>> list) {
-        mContext = context;
-        mList = list;
+        this.mContext = context;
+        this.mList = list;
+        this.mInflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -39,6 +49,38 @@ public class ZhujiListviewAdater extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ZhujiViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ZhujiViewHolder();
+            convertView = mInflater.inflate(R.layout.zhuji_list_item, null);
+            viewHolder.zhujiMainImg = (ImageView) convertView
+                    .findViewById(R.id.zhuji_main_img);
+            viewHolder.zhujiHeadImg = (BootstrapCircleThumbnail) convertView
+                    .findViewById(R.id.zhuji_head_img);
+            viewHolder.zhujiName = (TextView) convertView
+                    .findViewById(R.id.zhuji_name);
+            viewHolder.zhujiUpdateTime = (TextView) convertView
+                    .findViewById(R.id.zhuji_update_time);
+            viewHolder.zhujiMoreButton = (FontAwesomeText) convertView
+                    .findViewById(R.id.zhuji_more_button);
+            viewHolder.zhujiMainText = (TextView) convertView
+                    .findViewById(R.id.zhuji_main_text);
+            viewHolder.zhujiTagText = (TextView) convertView
+                    .findViewById(R.id.zhuji_tag_text);
+            viewHolder.zhujiZhanButton = (BootstrapButton) convertView
+                    .findViewById(R.id.zhuji_zhan_button);
+            viewHolder.zhujiCommentButton = (BootstrapButton) convertView
+                    .findViewById(R.id.zhuji_comment_button);
+            viewHolder.zhujiShareButton = (BootstrapButton) convertView
+                    .findViewById(R.id.zhuji_share_button);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ZhujiViewHolder) convertView.getTag();
+        }
+        viewHolder.zhujiMainImg.setImageResource((Integer)mList.get(position).get("mainImg"));
+        viewHolder.zhujiHeadImg.setImage((Integer)mList.get(position).get("headImg"));
+        
         return null;
     }
 }
--- app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiViewHolder.java
+++ app/src/main/java/com/example/root/libapp_v1/ZhujiListview/ZhujiViewHolder.java
@@ -3,13 +3,22 @@ package com.example.root.libapp_v1.ZhujiListview;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
+import com.beardedhen.androidbootstrap.FontAwesomeText;
+
 /**
  * Created by Yixin on 15-4-17.
  */
 public class ZhujiViewHolder {
     public ImageView zhujiMainImg;
-    public ImageView zhujiHeadImg;
+    public BootstrapCircleThumbnail zhujiHeadImg;
     public TextView zhujiName;
     public TextView zhujiUpdateTime;
-    public
+    public FontAwesomeText zhujiMoreButton;
+    public TextView zhujiMainText;
+    public TextView zhujiTagText;
+    public BootstrapButton zhujiZhanButton;
+    public BootstrapButton zhujiCommentButton;
+    public BootstrapButton zhujiShareButton;
 }
