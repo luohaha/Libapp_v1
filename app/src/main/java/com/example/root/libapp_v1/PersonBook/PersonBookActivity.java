56bd15d972f623160785c2f944747d1e97e27437
luohaha <18810541851@163.com>
2015-04-21 16:20:33 +0800

    update

--- app/src/main/java/com/example/root/libapp_v1/PersonBook/PersonBookActivity.java
+++ app/src/main/java/com/example/root/libapp_v1/PersonBook/PersonBookActivity.java
@@ -3,9 +3,16 @@ package com.example.root.libapp_v1.PersonBook;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import com.example.root.libapp_v1.HeadBar.HeadBar;
+import com.example.root.libapp_v1.PersonBook.PersonBookCommentListView.CommentListviewAdapter;
 import com.example.root.libapp_v1.R;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by Yixin on 15-4-21.
@@ -14,12 +21,22 @@ import com.example.root.libapp_v1.R;
  * And also he can send a book comment here.
  */
 public class PersonBookActivity extends Activity {
+    /**
+     * define var
+     * */
     HeadBar mHeadBar;
+    ArrayList<HashMap<String, Object>> mList;
+    ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personbook);
+        mListView = (ListView)findViewById(R.id.personbook_listview);
         setHeadBar();
+        setData();
+        CommentListviewAdapter adapter = new CommentListviewAdapter(this, mList,
+                R.layout.personbook_comment_item);
+        mListView.setAdapter(adapter);
     }
     /**
      * set the head bar
@@ -34,4 +51,19 @@ public class PersonBookActivity extends Activity {
             }
         });
     }
-}
+
+    /**
+     * set the data
+     * */
+    private void setData() {
+        mList = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            Date now = new Date();
+            DateFormat d1 = DateFormat.getDateInstance();
+            map.put("last_update_time", d1.format(now));
+            map.put("comment_title", "细品电音，提升逼格");
+            mList.add(map);
+        }
+    }
+ }
--- app/src/main/java/com/example/root/libapp_v1/PersonBook/PersonBookCommentListView/CommentListviewAdapter.java
+++ app/src/main/java/com/example/root/libapp_v1/PersonBook/PersonBookCommentListView/CommentListviewAdapter.java
@@ -1,9 +1,14 @@
 package com.example.root.libapp_v1.PersonBook.PersonBookCommentListView;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.example.root.libapp_v1.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,6 +49,38 @@ public class CommentListviewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(mItemLayout, null);
+            viewHolder = new ViewHolder();
+            viewHolder.lastUpdateTime = (TextView) convertView
+                    .findViewById(R.id.personbook_comment_time);
+            viewHolder.commentTitle = (TextView) convertView
+                    .findViewById(R.id.personbook_comment_title);
+            viewHolder.editComment = (BootstrapButton) convertView
+                    .findViewById(R.id.personbook_comment_edit_button);
+            viewHolder.shareComment = (BootstrapButton) convertView
+                    .findViewById(R.id.personbook_comment_share_button);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        viewHolder.lastUpdateTime.setText((String)mList.get(position).get("last_update_time"));
+        viewHolder.commentTitle.setText((String)mList.get(position).get("comment_title"));
+        viewHolder.editComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        viewHolder.shareComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return convertView;
     }
 }
