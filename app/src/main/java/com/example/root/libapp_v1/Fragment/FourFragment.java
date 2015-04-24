46bad93bf58a94a55a02d5bc6aa54d6740b261a9
luohaha <18810541851@163.com>
2015-04-24 10:01:42 +0800

    add personpage

--- app/src/main/java/com/example/root/libapp_v1/Fragment/FourFragment.java
+++ app/src/main/java/com/example/root/libapp_v1/Fragment/FourFragment.java
@@ -1,29 +1,63 @@
 package com.example.root.libapp_v1.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.example.root.libapp_v1.HeadBar.HeadBar;
 import com.example.root.libapp_v1.R;
+import com.example.root.libapp_v1.UserLogin.LoginActivity;
 
 /**
  * Created by Yixin on 15-3-26.
  * 我的
  */
-public class FourFragment extends FatherFragment {
-    //public String initContent() {
-      //  return "four!!!";
-    //}
+public class FourFragment extends FatherFragment{
+
+    /**
+     * define var
+     * */
     private HeadBar headBar;
+    private LinearLayout mBooksDetail;
+    private LinearLayout mRecordDetail;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment4, null);
-       // TextView textView = (TextView) view.findViewById(R.id.txt_content);
+        return view;
+    }
+    /**
+     * init the head bar
+     * */
+    private void initHeadBar() {
         headBar = (HeadBar)this.getActivity().findViewById(R.id.head_bar);
         headBar.setTitleText("我的");
-        return view;
+    }
+    private void initLayout() {
+        mBooksDetail = (LinearLayout) this.getActivity()
+                .findViewById(R.id.personpage_books_detail);
+        mRecordDetail = (LinearLayout) this.getActivity()
+                .findViewById(R.id.personpage_record_detail);
+        mBooksDetail.setOnClickListener(new View.OnClickListener() {
+            /**
+             * when the book detail layout is clicked, then
+             * */
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        mRecordDetail.setOnClickListener(new View.OnClickListener() {
+            /**
+             * when the record detail layout is clicked, then
+             * */
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
