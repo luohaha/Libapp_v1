75442e5705b14bbffc9ec84955da56e2c8d08c87
luohaha <18810541851@163.com>
2015-04-25 10:31:07 +0800

    add firstfragment

---
+++ app/src/main/java/com/example/root/libapp_v1/Fragment/FirstFragmentAdapter/FirstFragmentAdapter.java
@@ -0,0 +1,62 @@
+package com.example.root.libapp_v1.Fragment.FirstFragmentAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.root.libapp_v1.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Yixin on 15-4-25.
+ */
+public class FirstFragmentAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<HashMap<String, Object>> mList;
+
+    public FirstFragmentAdapter(Context mContext, ArrayList<HashMap<String, Object>> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        FirstFragmentViewHolder firstFragmentViewHolder = null;
+        if (convertView == null) {
+            firstFragmentViewHolder = new FirstFragmentViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.small_item, null);
+            firstFragmentViewHolder.imageView = (ImageView) convertView
+                    .findViewById(R.id.firstfragment_imageview);
+            firstFragmentViewHolder.textView = (TextView) convertView
+                    .findViewById(R.id.firstfragment_textview);
+            convertView.setTag(firstFragmentViewHolder);
+        } else {
+            firstFragmentViewHolder = (FirstFragmentViewHolder) convertView.getTag();
+        }
+        firstFragmentViewHolder.imageView.setImageResource((Integer)mList.get(position).get("image"));
+        firstFragmentViewHolder.textView.setText((String) mList.get(position).get("text"));
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+}
---
+++ app/src/main/java/com/example/root/libapp_v1/Fragment/FirstFragmentAdapter/FirstFragmentViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.root.libapp_v1.Fragment.FirstFragmentAdapter;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by root on 15-4-25.
+ */
+class FirstFragmentViewHolder {
+    ImageView imageView;
+    TextView textView;
+}
