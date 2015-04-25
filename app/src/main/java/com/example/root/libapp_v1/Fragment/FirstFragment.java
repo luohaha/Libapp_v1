9717c1b3a4892752ce24cde966edc68c01d6b6b2
luohaha <18810541851@163.com>
2015-04-25 15:57:12 +0800

    add first head tab back

--- app/src/main/java/com/example/root/libapp_v1/Fragment/FirstFragment.java
+++ app/src/main/java/com/example/root/libapp_v1/Fragment/FirstFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.RadioButton;
 
 import com.example.root.libapp_v1.Fragment.FirstFragmentAdapter.FirstFragmentAdapter;
 import com.example.root.libapp_v1.HeadBar.HeadBar;
@@ -25,6 +26,8 @@ public class FirstFragment extends FatherFragment {
 
     //
     private HeadBar headBar;
+    private RadioButton mAllButton;
+    private RadioButton mFriendButton;
     private GridView mGridView;
     private ArrayList<HashMap<String, Object>> mList;
     //array of books to show in gallery
@@ -43,10 +46,42 @@ public class FirstFragment extends FatherFragment {
         //create a view for fragment1
         View view = inflater.inflate(R.layout.fragment1, null);
         //find the things we want to show in view
-        mGridView=(GridView) view.findViewById(R.id.publicbook_gridview);
+        initRadioButton(view);
+        initHeadBar(view);
+        initData();
+        initGridView(view);
+        return view;
+    }
+
+    /**
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //
+
+    }
+    /**
+     * init the radiobutton, and set check first tab
+     * */
+    void initRadioButton(View view) {
+        mAllButton = (RadioButton) view.findViewById(R.id.firstfragment_tab1);
+        mFriendButton = (RadioButton) view.findViewById(R.id.firstfragment_tab2);
+        mAllButton.setChecked(true);
+    }
+    /**
+     * init the head bar
+     * */
+    void initHeadBar(View view) {
         headBar = (HeadBar)this.getActivity().findViewById(R.id.head_bar);
         headBar.setTitleText("飞书馆");
-        /*
+    }
+    /**
+     * init the test data
+     * */
+    void initData() {
+                /*
         * the array of item which need to show in gridview
         * it contains string and a picture
         * */
@@ -63,22 +98,17 @@ public class FirstFragment extends FatherFragment {
             map.put("text", tmp);// 按序号做ItemText
             mList.add(map);
         }
-        /*
+    }
+    /**
+     * init the grid view and set adapter for it
+     * */
+    void initGridView(View view) {
+        mGridView=(GridView) view.findViewById(R.id.publicbook_gridview);
+                /*
         * create a adapter
         * */
         FirstFragmentAdapter simple = new FirstFragmentAdapter(getActivity(), mList,
                 R.layout.firstfragment_gridview_item);
         mGridView.setAdapter(simple);
-        return view;
-    }
-
-    /**
-     * @param savedInstanceState
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //
-
     }
 }
